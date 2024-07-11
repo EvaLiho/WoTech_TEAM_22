108bab36ca4216008f3b05047c1d0fc8da14787a
EvaLiho <165931881+EvaLiho@users.noreply.github.com>
2024-07-11 20:05:24 +0300

    Create 11.07.2024.java

---
+++ 11.07.2024.java
@@ -0,0 +1,53 @@
+// Create an array in the endpoint, fill the array with data and access it from the URL
+// Create an object (new class, cheese or wine or whatever) in the endpoint, fill the object, access it from the URL
+
+
+package com.datorium.Datorium.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@CrossOrigin
+public class DatoriumApiApplication {
+
+	public static void main(String[] args) {
+		System.out.println("asd");
+		SpringApplication.run(DatoriumApiApplication.class, args);
+	}
+
+
+	@GetMapping("/ping")
+	public String ping() {
+		return "pong";
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+	@GetMapping("/sum")
+	public int sum(@RequestParam(value = "number1") int number1, @RequestParam(value = "number2") int number2){
+		return number1 + number2;
+	}
+	@GetMapping("/multiply")
+	public int multiply(@RequestParam(value = "number1") int number1, @RequestParam(value = "number2") int number2){
+		return number1 * number2;
+	}
+	@GetMapping("/array")
+	public int[] array(){
+		int[] array  = {1,2,3,4,5};
+		return array;
+	}
+	@GetMapping("/array2")
+	public int[] array(@RequestParam(value = "number1") int number1, @RequestParam(value = "number2") int number2){
+		int[] array  = {number1,number2};
+		return array;
+	}
+}
